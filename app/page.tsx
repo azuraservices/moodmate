66fa870921777a855362dab4f484dcac33d3b7b2
azuraservices <stanislao97s@gmail.com>
2024-11-11 11:19:24 +0100

    minor changes

--- app/page.tsx
+++ app/page.tsx
@@ -169,8 +169,7 @@ const getAIResponse = async (selectedEmojis: string[], language: 'en' | 'it'): P
 
   const url = 'https://api.groq.com/openai/v1/chat/completions';
   const headers = {
-    Authorization:
-      'Bearer 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',
+    Authorization: `Bearer ${process.env.NEXT_PUBLIC_API_KEY}`,
     'Content-Type': 'application/json',
   };
   const data = {
